adf429b6040ee2dda01c76e0b26aa4f1e75c500d
SaavedraJ24 <joaquinsaavedraesc@gmail.com>
2025-08-06 22:46:03 -0300

    I added styles to Cart Screen

--- src/global/colors.js
+++ src/global/colors.js
@@ -2,6 +2,7 @@ export const colors = {
     light: {
         background: "#f5e9e0",
         headline: "#b05e56",
+        darkBlue: "#11275a",
         text: "#3c3c3c",
         shadowColor: "#bb756e",
         tags: "#ff9292",
--- src/screens/cart/Cart.jsx
+++ src/screens/cart/Cart.jsx
@@ -14,8 +14,11 @@ const Cart = () => {
   const FooterComponent = () => (
     <View style={styles.footerContainer}>
       <TextDeliusSwashCapsRegular style={styles.totalTxt}>Total: ${total}</TextDeliusSwashCapsRegular>
-      <Pressable style={styles.confirmBtn}>
-        <TextDeliusBold style={styles.confirmBtnTxt}>Confirmar</TextDeliusBold>
+      <Pressable
+        style={({ pressed }) => [{ opacity: pressed ? 0.85: 1 }, styles.confirmBtn]}
+        onPress={() => null}
+      >
+        <TextDeliusBold style={styles.confirmBtnTxt}>Purchase</TextDeliusBold>
       </Pressable>
     </View>
   )
@@ -32,11 +35,12 @@ const Cart = () => {
       <View style={styles.cartDescription}>
         <TextDeliusSwashCapsRegular style={styles.title}>{item.title}</TextDeliusSwashCapsRegular>
         <TextDeliusSwashCapsRegular style={styles.description}>{item.description}</TextDeliusSwashCapsRegular>
-        <TextDeliusSwashCapsRegular style={styles.price}>Precio unitario: {item.price}</TextDeliusSwashCapsRegular>
-        <TextDeliusSwashCapsRegular style={styles.quantity}>Cantidad: {item.quantity}</TextDeliusSwashCapsRegular>
+        <TextDeliusSwashCapsRegular style={styles.price}>Unit price: {item.price}</TextDeliusSwashCapsRegular>
+        <TextDeliusSwashCapsRegular style={styles.quantity}>Quantity: {item.quantity}</TextDeliusSwashCapsRegular>
         <TextDeliusSwashCapsRegular style={styles.total}>Total: ${item.quantity * item.price}</TextDeliusSwashCapsRegular>
-        <Pressable onPress={() => dispatch(removeItems(item.id))}>
+        <Pressable onPress={() => dispatch(removeItems(item.id))} style={styles.deleteIconBox}>
           <Icon name='delete' size={24} color={colors.light.shadowColor} style={styles.deleteIcon} />
+          <TextDeliusSwashCapsRegular style={styles.deleteIconTxt}>--Delete product.</TextDeliusSwashCapsRegular>
         </Pressable>
       </View>
     </FlatCard >
@@ -50,11 +54,11 @@ const Cart = () => {
             data={cartItems}
             keyExtractor={item => item.id}
             renderItem={renderCartItem}
-            ListHeaderComponent={<TextDeliusBold style={styles.cartScreenTitle}>Tu carrito:</TextDeliusBold>}
+            ListHeaderComponent={<TextDeliusBold style={styles.cartScreenTitle}>Your cart:</TextDeliusBold>}
             ListFooterComponent={<FooterComponent />}
           />
           :
-          <TextDeliusSwashCapsRegular>Aún no hay productos en tu carrito.</TextDeliusSwashCapsRegular>
+          <TextDeliusSwashCapsRegular style={styles.txtNoProducts}>Not products in cart, yet.</TextDeliusSwashCapsRegular>
       }
     </>
   )
@@ -64,42 +68,86 @@ export default Cart;
 
 const styles = StyleSheet.create({
   footerContainer: {
-
+    alignItems: "center",
+    gap: 12,
+    paddingVertical: 12,
   },
   totalTxt: {
-
+    fontSize: 24,
+    color: colors.light.darkBlue,
   },
   confirmBtn: {
-
+    borderRadius: 8,
+    paddingHorizontal: 32,
+    paddingVertical: 8,
+    backgroundColor: colors.light.purchaseBtn,
   },
   confirmBtnTxt: {
-
+    color: colors.light.darkBlue,
+    fontSize: 24,
   },
   cartImage: {
-
+    height: 128,
+    width: 128,
+    position: 'absolute',
+    right: -160,
+    top: 80,
   },
   cartDescription: {
-
+    padding: 20,
   },
   title: {
-
+    fontSize: 24,
+    color: colors.light.headline,
   },
   description: {
-
+    fontSize: 16,
+    color: colors.light.text,
+    padding: 8,
   },
   price: {
-
+    color: colors.light.text,
+    fontSize: 16,
+    padding: 8,
   },
   quantity: {
-
+    color: colors.light.text,
+    fontSize: 16,
+    padding: 8,
   },
   total: {
-
+    color: colors.light.text,
+    fontSize: 16,
+    padding: 8,
+  },
+  deleteIconBox: {
+    flexDirection: "row",
+    alignItems: "center",
   },
   deleteIcon: {
-
+    height: 44,
+    paddingVertical: 8,
+    paddingLeft: 8,
+  },
+  deleteIconTxt: {
+    fontSize: 20,
+    color: colors.light.shadowColor,
+    textDecorationLine: "underline",
+    paddingLeft: 4,
   },
   cartScreenTitle: {
-
+    width: "100%",
+    textTransform: "uppercase",
+    fontSize: 36,
+    paddingTop: 12,
+    textAlign: "center",
+    color: colors.light.darkBlue,
   },
+  txtNoProducts: {
+    marginVertical: 100,
+    fontSize: 28,
+    alignSelf: "center",
+    color: colors.light.text,
+    textDecorationLine: "underline"
+  }
 })
